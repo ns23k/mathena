5173593d0b9b366f0f86d379a9a3bb2900b84444
Naman <namansharma232009@gmail.com>
2022-05-18 17:03:47 +0530

    first commit lets go

---
+++ mathena/basic/__init__.py
@@ -0,0 +1,1 @@
+from mathena.basic.primefactorization import prime_factorization
---
+++ mathena/basic/primefactorization.py
@@ -0,0 +1,27 @@
+import math
+from typing import *
+
+
+def prime_factorization(num: int) -> List[int]:
+    n = num
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n = n / 2
+
+    # n must be odd at this point
+    # so a skip of 2 ( i = i + 2) can be used
+    for i in range(3, int(math.sqrt(n))+1,2):
+
+        # while i divides n , print i and divide n
+        while n % i == 0:
+            factors.append(i)
+            n = n / i
+
+    # Condition if n is a prime
+    # number greater than 2
+    if n > 2:
+        factors.append(n)
+
+    return factors
+
